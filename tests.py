ff8c4c5d301775b895cd9bbd83027cf3a47f00cf
Raphael Gontijo Lopes <raphaelgontijolopes@gmail.com>
2015-02-12 12:02:38 -0500

    tests.py now has instructions. sampleGraph incorporated into it

--- tests.py
+++ tests.py
@@ -3,14 +3,14 @@ from score import score, diagnose
 from utils import viz
 from randomPermutation import getRandom
 
-matrix = AdjacencyMatrix(7);
-matrix.addVertices(["A", "B", "C", "D", "E", "F", "G"])
-matrix.addEdge("D", "A")
-matrix.addEdge("D", "F")
-matrix.addEdge("D", "B")
-matrix.addEdge("B", "C")
-matrix.addEdge("B", "E")
-matrix.addEdge("F", "G")
+matrix7 = AdjacencyMatrix7(7);
+matrix7.addVertices(["A", "B", "C", "D", "E", "F", "G"])
+matrix7.addEdge("D", "A")
+matrix7.addEdge("D", "F")
+matrix7.addEdge("D", "B")
+matrix7.addEdge("B", "C")
+matrix7.addEdge("B", "E")
+matrix7.addEdge("F", "G")
 
 goodSolution = {
     "A": (5, 10),
@@ -32,14 +32,38 @@ badSolution = {
     "G": (15, 15)
 }
 
+matrix26 = AdjacencyMatrix(26)
+matrix26.addVertices(["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O"])
+matrix26.addEdge("A", "J")
+matrix26.addEdge("A", "K")
+matrix26.addEdge("J", "I")
+matrix26.addEdge("I", "H")
+matrix26.addEdge("I", "C")
+matrix26.addEdge("I", "E")
+matrix26.addEdge("K", "L")
+matrix26.addEdge("B", "D")
+matrix26.addEdge("C", "D")
+matrix26.addEdge("C", "O")
+matrix26.addEdge("L", "O")
+matrix26.addEdge("L", "F")
+matrix26.addEdge("O", "G")
+matrix26.addEdge("E", "N")
+matrix26.addEdge("G", "H")
+matrix26.addEdge("D", "M")
+matrix26.addEdge("M", "F")
+
 import hillclimb
 import simulatedannealing
-
 import sampleGraph
 
-matrix = sampleGraph.matrix
+# PICK YOUR STARTING MATRIX
+# matrix = matrix4
+matrix = matrix26
 
 randSolution = getRandom(matrix.vertices)
+
+# FOR HILL CLIMBING, UNCOMMENT FIRST LINE.
+# FOR SIMU ANNEALIN, UNCOMMENT SECOND LINE.
 # solution, score, tries = hillclimb.climbhill(matrix, randSolution)
 solution, fScore = simulatedannealing.simulateanneal(matrix, randSolution)
 
